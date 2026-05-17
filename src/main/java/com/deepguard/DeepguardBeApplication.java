ef8e10877715496b3d1587cf098362ded4c9f439
Nguyễn Bá Đoàn <nguyenbadoan170504@gmail.com>
2026-05-17 07:34:19 +0700

    Initiation Project

---
+++ src/main/java/com/deepguard/DeepguardBeApplication.java
@@ -0,0 +1,13 @@
+package com.deepguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepguardBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepguardBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepguard/DeepguardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepguardBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
